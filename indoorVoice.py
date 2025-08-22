63424bedad092c7749566644187143c39bf52f1a
harsh-ai <yourgithubemail@gmail.com>
2025-08-22 20:24:22 +0530

    First commit - Added Day-1 files

---
+++ indoorVoice.py
@@ -0,0 +1,5 @@
+# A program in Python that prompts the user for input and then outputs that same input in lowercase. 
+
+name = input("Enter your name : ")
+name = name.lower()
+print("Name =",name)
